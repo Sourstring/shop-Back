ee83ade4d82b0e9d6796c6705c5ddccf734ae270
sourstring <juanibianchi4@gmail.com>
2023-09-25 19:26:09 -0300

    Added crossOrigin to CategoryController

--- src/main/java/com/example/shop/controller/CategoryController.java
+++ src/main/java/com/example/shop/controller/CategoryController.java
@@ -14,16 +14,19 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/all")
     public ResponseEntity<?> getAllCategories() {
         return new ResponseEntity<>(categoryService.findAll(), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/{nombre}")
     public ResponseEntity<?> getCategoryByName(@PathVariable String nombre) {
         return new ResponseEntity<>(categoryService.findByNombre(nombre), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/create")
     public ResponseEntity<?> createCategory(@RequestBody CategoryDto category) {
         return new ResponseEntity<>(categoryService.save(category.getNombre()), HttpStatus.CREATED);
